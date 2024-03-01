2d5810db956a3a03247c38a75e623c1393e49f7b
Reizora <lauglaugjefter@gmail.com>
2024-03-01 13:51:45 +0800

    optimized the initial commit.

--- src/main/java/me/reizora/dev/afkplugin/AfkPlugin.java
+++ src/main/java/me/reizora/dev/afkplugin/AfkPlugin.java
@@ -1,13 +1,19 @@
 package me.reizora.dev.afkplugin;
 
+import me.reizora.dev.afkplugin.commands.playerCommands;
+import me.reizora.dev.afkplugin.listeners.playerEvents;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AfkPlugin extends JavaPlugin {
-
     @Override
     public void onEnable() {
         // Plugin startup logic
+        playerEvents player = new playerEvents();
         afkMonitor afk = new afkMonitor(this);
+
+        getServer().getPluginManager().registerEvents(player, this);
+        getCommand("setspawn").setExecutor(new playerCommands());
+
         afk.startIdleCheckTask();
     }
 
